2bcbfcd4842b9344b6590b0572158c54cbf8f48e
cookiewho <dinkwho@gmail.com>
2020-09-27 15:34:02 -0700

    finished problem 2 after a lot of debbugging

--- CST370/hw5_2.cpp
+++ CST370/hw5_2.cpp
@@ -10,31 +10,78 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <algorithm>
+#include <climits>
 using namespace std;
+
+void display(int a[][0], int n) 
+{ 
+	for (int i = 0; i < n; i++) 
+	{ 
+		cout << a[i] << " "; 
+	} 
+	cout << endl; 
+} 
 
 int main(){
     
     int verticies, edges, x, y, cost, start, z = 0;
     cin >> verticies >> edges;
-    int matrix[verticies][verticies] = {0};
+    int v_size = verticies-1;
+    int matrix[verticies][verticies] = {};
+    int vertexs[v_size];
     while(z < edges){
         cin >> x >> y >> cost;
-        if(x < verticies && y < verticies && x != y){
-            matrix[x][y] = cost;
-        }
+        matrix[x][y] = cost;
         z++;
     }
     cin >> start;
-    for(int x=0 ; x<verticies ; x++){
-        cout << x;
-        for(int y=0 ; y<verticies ; y++){
-            if (matrix[x][y] != 0){
-                cout << "--" << matrix[x][y] << "->" << y ;
+    int count = 0;
+    for(int z = 0; z <v_size; z++){
+        if(count == start){count++;}
+            {
+            vertexs[z] = count;
+            count++;
+        }
+    }
+    int min_count=INT_MAX;
+    string min_path = "";
+    //ALGORITHM MAGIC HERE
+    do{
+        string s_temp = std::to_string(start) + "->";
+        int count_temp = 0;
+        int prev = start;
+        for(int x=0 ; x<v_size; x++){  //one permutation
+            int val = matrix[prev][vertexs[x]];
+            if(val == 0){
+                count_temp = 0;
+                break;
+            }
+            else{
+                count_temp += val;
+                prev = vertexs[x];
+                s_temp +=  std::to_string(vertexs[x]) + "->";
             }
         }
-        cout << endl;
+        int val = matrix[prev][start];//messy check to see if we can go back to start
+        if(val == 0){
+            count_temp = 0;
+        }
+        else{
+            if(count_temp != 0){
+                count_temp += val;
+                s_temp += std::to_string(start);
+                if(count_temp < min_count){
+                    min_count = count_temp;
+                    min_path = s_temp;
+                }
+            }
+        }
+    }while(next_permutation(vertexs, vertexs+v_size));//gets all permutations
+    if (min_count == INT_MAX || min_count == 0){
+        min_path = "";
+        min_count = -1;
     }
-    //ALL ABOVE IS INPUT
+    cout<< "Path:"<<min_path << endl << "Cost:" << min_count << endl;
     
-    //ALGORITHM MAGIC HERE
 }
