8822b87e4721177b864832a83ca91fb1be9aa79f
cookiewho <dinkwho@gmail.com>
2020-10-01 23:11:18 -0700

    finished python version of the week 6 hw prob for cst 370

--- CST370/hw6_1.py
+++ CST370/hw6_1.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 vs = int(input())
 graph = [[]] *vs
-print (graph)
 cities_ind = {}
 ind_cities = {}
 ans = OrderedDict()
@@ -9,28 +8,35 @@ for x in range(vs):
   temp = input()
   cities_ind[temp] = x
   ind_cities[x] = temp
-print(cities_ind)
+
+
 edges = int(input())
 for x in range(edges):
   temp = [y for y in input().split()]
   graph[cities_ind[temp[0]]].append(cities_ind[temp[1]])
+
+
 curr = cities_ind[input()]
 hops_allowed = int(input())
-visited = [curr]
+visited= [0]*len(graph)
+visited[curr] = 1
 hops_taken = 0
+if hops_allowed >0:
+  ans[ind_cities[curr]] = 0
+
+
 q = [curr]
-while((len(q) > 0) and hops_taken <= hops_allowed):
-  curr = q.pop()
-  ans[ind_cities[curr]] = hops_taken
+while(len(q) > 0):
+  curr = q.pop(0)
   for x in range(len(graph[curr])):
     neighbor = graph[curr][x]
-    if neighbor in visited:
-      print(neighbor, "already here")
-    else:
-      visited.append(neighbor)
+    if visited[neighbor] == 0:
+      visited[neighbor]=1
       q.append(neighbor)
-  print("APPENDED", q)
-  print("VISITED", visited)
-  hops_taken+=1
-for key,val in ans.items():
-  print(key, val)
+      # print("prev", ans[ind_cities[curr]])
+      hops_taken = ans[ind_cities[curr]]+1
+      if hops_taken<=hops_allowed:
+        ans[ind_cities[neighbor]] = hops_taken
+print()
+for key in sorted(ans.keys()):
+  print(key, ans[key])
